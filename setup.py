c7d8ee13e568e8dc8d0a1e4042a4c6e63dc85a3f
mahfida <mahrukhfida@gmail.com>
2026-01-15 13:19:58 +0000

    setup files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="Medical ChatBot Project",
+    version="0.0.0",
+    author="Mah-Rukh Fida",
+    author_email="mahrukhfida@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
